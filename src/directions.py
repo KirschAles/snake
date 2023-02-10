883dd8197aa7a07b92a6449f468758f06e3965c3
Ales Kirsch <kirschales@gmail.com>
2023-02-10 20:41:49 +0100

    Added Directins

---
+++ src/directions.py
@@ -0,0 +1,32 @@
+class Vertical:
+    def __init__(self):
+        pass
+
+
+class Horizontal:
+    def __init__(self):
+        pass
+
+
+class Left(Horizontal):
+    def __init__(self):
+        super().__init__()
+        pass
+
+
+class Right(Horizontal):
+    def __init__(self):
+        super().__init__()
+        pass
+
+
+class Up(Vertical):
+    def __init__(self):
+        super().__init__()
+        pass
+
+
+class Down(Vertical):
+    def __init__(self):
+        super().__init__()
+        pass
