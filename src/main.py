2fc723d61e1d12baa4b8d4aacbc8037b1e448946
Ales Kirsch <kirschales@gmail.com>
2023-02-10 14:52:11 +0100

    Added class Movable rect

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import pygame
 import pygame.locals
+from movable import MovableRect
 import sys
 
 pygame.init()
@@ -9,14 +10,19 @@ rect_color = (255, 0, 0)
 
 exiting = False
 pygame.display.set_caption('My Game')
-
+FPS = pygame.time.Clock()
+FPS.tick(60)
 canvas = pygame.display.set_mode((500, 500))
 # game loop
+rect = MovableRect(0, 0, 60, 60)
 while not exiting:
     canvas.fill(color)
     for event in pygame.event.get():
+        print(event)
         if event.type == pygame.locals.QUIT:
             exiting = True
-
-    pygame.draw.rect(canvas, rect_color, pygame.Rect(200, 200, 100, 100))
+        if event.type == pygame.locals.KEYDOWN:
+            print('here')
+            rect.update(event)
+    pygame.draw.rect(canvas, rect_color, rect.get_draw())
     pygame.display.update()
--- src/movable.py
+++ src/movable.py
@@ -1,29 +1,33 @@
 from pygame import Rect
-
+import pygame
+import pygame.locals as key
+from math import ceil
 
 class Movable:
     def __init__(self, x_pos: int, y_pos: int):
         self.x = x_pos
         self.y = y_pos
 
-    def move(self, x_change, y_change):
-        self.x = self.x + x_change
-        self.y = self.y + y_change
+    def move(self, x_change: float = 0, y_change: float = 0):
+        self.x = ceil(self.x + x_change)
+        self.y = ceil(self.y + y_change)
 
-    def move_left(self, left_change):
+    def move_left(self, left_change: int):
         self.move(-left_change, 0)
 
-    def move_right(self, right_change):
+    def move_right(self, right_change: int):
         self.move(right_change)
 
-    def move_up(self, up_change):
+    def move_up(self, up_change: int):
         self.move(0, -up_change)
 
-    def move_down(self, down_change):
+    def move_down(self, down_change: int):
         self.move(0, down_change)
 
 
 class MovableRect(Movable):
+    base_speed = 1
+
     def __init__(self,
                  x_pos: int,
                  y_pos: int,
@@ -32,6 +36,32 @@ class MovableRect(Movable):
         super().__init__(x_pos, y_pos)
         self.width = width
         self.height = height
+        self.speed = ceil(self.base_speed*width)
 
     def get_draw(self):
         return Rect(self.x, self.y, self.width, self.height)
+
+    def left(self):
+        super().move_left(self.speed)
+
+    def right(self):
+        super().move_right(self.speed)
+
+    def up(self):
+        super().move_up(self.speed)
+
+    def down(self):
+        super().move_down(self.speed)
+
+    def update(self, event):
+        if event.key == pygame.K_LEFT or event.key == ord('a'):
+            self.left()
+        if event.key == pygame.K_UP or event.key == ord('w'):
+            self.up()
+        if event.key == pygame.K_DOWN or event.key == ord('s'):
+            self.down()
+        if event.key == pygame.K_RIGHT or event.key == ord('d'):
+            self.right()
+
+    def __str__(self):
+        return str(self.x) + ', ' + str(self.y)
