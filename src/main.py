f3ee2f6e088197cb67e4fe03e1b4c552cbe59d49
Ales Kirsch <kirschales@gmail.com>
2023-02-12 18:07:38 +0100

    Added main menu

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import pygame
 import pygame.locals
+import pygame_gui
 from movable import MovableRect
 import sys
 from world import World
@@ -25,7 +26,6 @@ def game_loop(canvas_height: int,
               block_size: int,
               blocks_per_second: float):
     exiting = False
-    pygame.display.set_caption('Snake')
     FPS = pygame.time.Clock()
     FPS.tick(60)
 
@@ -63,6 +63,44 @@ canvas_width = width * block_size
 canvas_height = height * block_size
 canvas_colour = (200, 200, 200)
 
+
 pygame.init()
 
-game_loop(canvas_height, canvas_width, canvas_colour, height, width, block_size, blocks_per_second)
+window_surface = pygame.display.set_mode((canvas_width, canvas_height))
+pygame.display.set_caption('Snake')
+background = pygame.Surface((canvas_width, canvas_height))
+background.fill(pygame.Color('#000000'))
+manager = pygame_gui.UIManager((canvas_width, canvas_height))
+
+single_player = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((300, 200), (200, 50)),
+                                            text='Single Player',
+                                            manager=manager)
+exit_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((300, 260), (200, 50)),
+                                           text='Exit',
+                                           manager=manager)
+
+exiting = False
+clock = pygame.time.Clock()
+
+while not exiting:
+    time_delta = clock.tick(60) / 1000.0
+    for event in pygame.event.get():
+        if event.type == pygame.locals.QUIT:
+            exiting = True
+        if event.type == pygame_gui.UI_BUTTON_PRESSED:
+            if event.ui_element == single_player:
+                game_loop(canvas_height,
+                          canvas_width,
+                          canvas_colour,
+                          height,
+                          width,
+                          block_size,
+                          blocks_per_second)
+            if event.ui_element == exit_button:
+                exiting = True
+        manager.process_events(event)
+
+    manager.update(time_delta)
+    window_surface.blit(background, (0, 0))
+    manager.draw_ui(window_surface)
+    pygame.display.update()
